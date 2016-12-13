8a51613dbc7e79ef55fa6b25e9f212b2b8a178a9
Mental Arena <caperaven@icloud.com>
2016-12-13 12:31:56 +0200

    autocomplete

--- src/views/validation/validation.js
+++ src/views/validation/validation.js
@@ -1,5 +1,41 @@
 
 // @flow
 export class Validation {
+    // https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Forms/Data_form_validation
 
+    form: any;
+    invalidAction: any;
+
+    constructor() {
+        this.invalidAction = this.invalid.bind(this);
+    }
+
+    attached() {
+        this.form = document.getElementById("form");
+        this.form.addEventListener("keyup", this.invalidAction);
+    }
+
+    detached() {
+        this.form.removeEventListener("keyup", this.invalidAction)
+    }
+
+    invalid(event) {
+        return new Promise(resolve => {
+            const isValid = event.target.checkValidity();
+
+            if (!isValid) {
+                event.target.setAttribute("data-error", event.target.validationMessage);
+            }
+            else {
+                event.target.removeAttribute("data-error");
+            }
+
+            resolve();
+        })
+    }
+
+    validate() {
+        const isValid = this.form.checkValidity();
+        console.log(this.form.validity);
+    }
 }
